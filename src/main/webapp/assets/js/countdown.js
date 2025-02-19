164fdb48fd9fa0c7b3a1429229c71a80847491a8
natsu <alaoui.hamza2002@gmail.com>
2025-02-19 22:19:46 +0100

    directories problem fixed

--- src/main/webapp/assets/js/countdown.js
+++
@@ -1,9 +0,0 @@
-let seconds = 5;  // Initial countdown value
-function updateCountdown() {
-    if (seconds >= 0) {
-        document.getElementById("countdown").innerText = seconds;
-        seconds--;
-        setTimeout(updateCountdown, 1000);  // Call function every second
-    }
-}
-window.onload = updateCountdown;  // Start countdown when page loads
